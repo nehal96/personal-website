12b989bc181e7bf82ea8ab814202dcc956bb1dd5
Nehal Udyavar <nehaludyavar@gmail.com>
2020-02-02 16:59:36 -0800

    Barebone sidebar + main layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
+import Sidebar from "./Sidebar";
+import Main from "./Main";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">Hi there</header>
+    <div className="app">
+      <Sidebar />
+      <Main />
     </div>
   );
 }
---
+++ src/Main.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./Main.module.css";
+
+const Main = () => {
+  return <div className={styles.container}>Main</div>;
+};
+
+export default Main;
